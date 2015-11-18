1d8922565cceede63e66697fe8a6768b23f9b545
taguchi-n2310 <taguchi-n2310@afrel.co.jp>
2015-11-18 18:06:47 +0900

    iot-devmgmt_の追加（モバイルサービス）

---
+++ dev/iot-devmgmt_/iot_devmgmtService/Controllers/TodoItemController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Controllers;
+using System.Web.Http.OData;
+using Microsoft.WindowsAzure.Mobile.Service;
+using iot_devmgmtService.DataObjects;
+using iot_devmgmtService.Models;
+
+namespace iot_devmgmtService.Controllers
+{
+    public class TodoItemController : TableController<TodoItem>
+    {
+        protected override void Initialize(HttpControllerContext controllerContext)
+        {
+            base.Initialize(controllerContext);
+            iot_devmgmtContext context = new iot_devmgmtContext();
+            DomainManager = new EntityDomainManager<TodoItem>(context, Request, Services);
+        }
+
+        // GET tables/TodoItem
+        public IQueryable<TodoItem> GetAllTodoItems()
+        {
+            return Query();
+        }
+
+        // GET tables/TodoItem/48D68C86-6EA6-4C25-AA33-223FC9A27959
+        public SingleResult<TodoItem> GetTodoItem(string id)
+        {
+            return Lookup(id);
+        }
+
+        // PATCH tables/TodoItem/48D68C86-6EA6-4C25-AA33-223FC9A27959
+        public Task<TodoItem> PatchTodoItem(string id, Delta<TodoItem> patch)
+        {
+            return UpdateAsync(id, patch);
+        }
+
+        // POST tables/TodoItem
+        public async Task<IHttpActionResult> PostTodoItem(TodoItem item)
+        {
+            TodoItem current = await InsertAsync(item);
+
+            Dictionary<string, string> data = new Dictionary<string, string>()
+    {
+        { "message", item.Text},
+        { "floor", "3"},
+        { "Room", "Test"}
+    };
+            GooglePushMessage message = new GooglePushMessage(data, TimeSpan.FromHours(1));
+
+            try
+            {
+                var result = await Services.Push.SendAsync(message);
+                Services.Log.Info(result.State.ToString());
+            }
+            catch (System.Exception ex)
+            {
+                Services.Log.Error(ex.Message, null, "Push.SendAsync Error");
+            }
+            return CreatedAtRoute("Tables", new { id = current.Id }, current);
+        }
+
+        // DELETE tables/TodoItem/48D68C86-6EA6-4C25-AA33-223FC9A27959
+        public Task DeleteTodoItem(string id)
+        {
+            return DeleteAsync(id);
+        }
+    }
+}
---
+++ dev/iot-devmgmt_/iot_devmgmtService/DataObjects/DeviceStatus.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace iot_devmgmtService.DataObjects
+{
+    public class DeviceStatus
+    {
+    }
+}
